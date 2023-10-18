85c6649fd5386ea3ea2856f403fc732ae168867b
kecknews <kecknews@yandex.ru>
2023-10-18 08:40:56 +0500

    тестовое задание 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,57 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int a = 1;
+
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int a = 1;
+        if (size() < 10) {
+            storage[size()] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++)
+            if (uuid == String.valueOf(storage[i])) {
+                return storage[i];
+            }
         return null;
     }
 
     void delete(String uuid) {
+        Resume[] newStorage = new Resume[10];
+        for (int i = 0, j = 0; i < size(); i++) {
+            if (String.valueOf(storage[i]) != uuid)
+                newStorage[j++] = storage[i];
+        }
+        storage = newStorage;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] getStorage = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            getStorage[i] = storage[i];
+        }
+        return getStorage;
     }
 
     int size() {
-        return 0;
+
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                count++;
+            }
+        }
+        return count;
+
     }
 }
