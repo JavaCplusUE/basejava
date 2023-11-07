dc4aec5447cddaa0751f7ec3902ba99a239a7dd4
JavaCplusUE <JavaCplusUE@yandex.ru>
2023-11-07 11:13:33 +0500

    Ваш комментарий к коммиту

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -26,12 +26,27 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        Resume[] newStorage = new Resume[10];
-        for (int i = 0, j = 0; i < size(); i++) {
-            if (String.valueOf(storage[i]) != uuid)
-                newStorage[j++] = storage[i];
+//        Resume[] newStorage = new Resume[10];
+//        for (int i = 0, j = 0; i < size(); i++) {
+//            if (String.valueOf(storage[i]) != uuid)
+//                newStorage[j++] = storage[i];
+//        }
+//        storage = newStorage;
+
+        for (int i = 0; i < size() - 1; i++) {
+            for (int j = 0; j < size() - i - 1; j++) {
+                Resume temp = storage[j+1];
+                if (storage[j].uuid == uuid){
+                    storage[j].uuid = null;
+                }
+                if (storage[j].uuid == null) {
+                    storage[j].uuid = temp.uuid;
+                    storage[j+1].uuid = null;
+                }
+            }
         }
-        storage = newStorage;
+
+
     }
 
     /**
