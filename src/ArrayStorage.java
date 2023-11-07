458095a14919f8033e4432c1eb0fd3f5e36b2bac
JavaCplusUE <JavaCplusUE@yandex.ru>
2023-11-07 11:51:17 +0500

    Поправил удаление

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,6 +3,8 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int sizeStorage;
+
 
     void clear() {
         for (int i = 0; i < size(); i++) {
@@ -15,6 +17,7 @@ public class ArrayStorage {
         if (size() < 10) {
             storage[size()] = r;
         }
+        sizeStorage += 1;
     }
 
     Resume get(String uuid) {
@@ -26,25 +29,21 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-//        Resume[] newStorage = new Resume[10];
-//        for (int i = 0, j = 0; i < size(); i++) {
-//            if (String.valueOf(storage[i]) != uuid)
-//                newStorage[j++] = storage[i];
-//        }
-//        storage = newStorage;
-
-        for (int i = 0; i < size() - 1; i++) {
-            for (int j = 0; j < size() - i - 1; j++) {
-                Resume temp = storage[j+1];
-                if (storage[j].uuid == uuid){
-                    storage[j].uuid = null;
+        for (int i = 0; i < sizeStorage; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i].uuid = null;
+                for (int j = i; j < sizeStorage - i - 1; j++) {
+                    Resume temp = storage[j + 1];
+                    if (storage[j].uuid == null) {
+                        storage[j].uuid = temp.uuid;
+                        storage[j + 1].uuid = null;
+                    }
                 }
-                if (storage[j].uuid == null) {
-                    storage[j].uuid = temp.uuid;
-                    storage[j+1].uuid = null;
-                }
+                sizeStorage -= 1;
+                break;
             }
         }
+
 
 
     }
@@ -61,14 +60,13 @@ public class ArrayStorage {
     }
 
     int size() {
-
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                count++;
-            }
-        }
-        return count;
+        //        int count = 0;
+//        for (int i = 0; i < storage.length; i++) {
+//            if (storage[i] != null) {
+//                count++;
+//            }
+//        }
+        return sizeStorage;
 
     }
 }
